59717660168852caae77b2789dce8e45b6548050
togoc <49116563+togoc@users.noreply.github.com>
2019-12-15 02:49:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    dbURI: "mongodb://106.13.184.92:27017/react-article",
+    dbURI: "mongodb://106.13.184.92:27017/www",
     options: {
         useNewUrlParser: true, //使用客户端
         useUnifiedTopology: true, //报错
-        authSource: "react-article",
-        user: "react",
+        authSource: "www",
+        user: "www",
         pass: "98d1e3bcdd"
     }
 }
