06926af8f14c5f5d16683a51b8ee9d32b208f0ef
togoc <49116563+togoc@users.noreply.github.com>
2019-12-31 02:07:31 +0800

    update

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const app = express()
 
 //启用gzip
 app.use(compression());
+// app.use(compression({ filter: shouldCompress }))
+
+function shouldCompress(req, res) {
+    if (req.headers['x-no-compression']) {
+        // 这里就过滤掉了请求头包含'x-no-compression'
+        return false
+    }
+
+    return compression.filter(req, res)
+}
 
 //post body
 const bodyparser = require('body-parser');
