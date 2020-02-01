354b1adaedddea80f02aade1de2c4cfbd2716104
Your Name <you@example.com>
2020-02-01 14:29:47 +0800

    re

--- index/router.js
+++ index/router.js
@@ -2,14 +2,11 @@ const express = require('express');
 const DB = require('./db')
 const router = express.Router()
 module.exports = (app) => {
-    router.get('/demolist', (req, res) => {
+    router.get('/index/demolist', (req, res) => {
         DB.demolist.find({}).then(list => {
             res.status(200).json(list);;
         })
     });
-
     app.use('/index', express.static(__dirname));
     app.use(router);
 }
-
-
