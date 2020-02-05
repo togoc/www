84ad1b5157e9fda64b62253f19bd0cd2c7f5639e
togoc <oncwnuDRPW2KUww1bxVRTJZJIaq0@git.weixin.qq.com>
2020-02-05 15:13:06 +0800

    file url

--- index/router.js
+++ index/router.js
@@ -9,11 +9,10 @@ module.exports = (app) => {
             res.status(200).json(list);;
         })
     });
-    console.log(path.resolve('./utils/dev/dev.txt'))
-    console.log(path.resolve('utils/dev/dev.txt'))
+
     router.get('/chart', (req, res) => {
-        let data = fs.readFileSync(path.resolve('./utils/dev/dev.txt'), 'utf8')
-        res.send(data);
+        let data = fs.readFileSync(path.resolve('utils/dev/dev.txt'), 'utf8')
+        res.send(path.resolve('utils/dev/dev.txt'));
     });
 
     app.use('/index', express.static(__dirname));
