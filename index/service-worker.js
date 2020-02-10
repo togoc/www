958609f47d3cf08b74f0de71f4e329f32f268316
togoc <oncwnuDRPW2KUww1bxVRTJZJIaq0@git.weixin.qq.com>
2020-02-10 17:06:43 +0800

    cdn index

--- index/service-worker.js
+++ index/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7c758162da7db21e74e474690999e10a.js"
+  "./precache-manifest.4390036001c409826fe60c081b5e36e7.js"
 );
 
 self.addEventListener('message', (event) => {
