bc95459c38f2a2978d7ded2b842f60fd8e0762dc
togoc <oncwnuDRPW2KUww1bxVRTJZJIaq0@git.weixin.qq.com>
2020-03-07 23:58:11 +0800

    rebuild index

---
+++ index/precache-manifest.5ba5f93b679080d10c6cc02079c6b00f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5189405fea419a0a6ad6c55c3535c59",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a5591994c79ddaf38e01",
+    "url": "./static/css/2.d73c4648.chunk.css"
+  },
+  {
+    "revision": "d0d657b6f78f736a4ec1",
+    "url": "./static/css/main.a026d46e.chunk.css"
+  },
+  {
+    "revision": "a5591994c79ddaf38e01",
+    "url": "./static/js/2.1fc645c4.chunk.js"
+  },
+  {
+    "revision": "4d942bb6f94119c6d39c238d0718fe73",
+    "url": "./static/js/2.1fc645c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0d657b6f78f736a4ec1",
+    "url": "./static/js/main.51bff97c.chunk.js"
+  },
+  {
+    "revision": "f9d100b48ad0f300c15c",
+    "url": "./static/js/runtime-main.5acff73e.js"
+  }
+]);
